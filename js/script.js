225b1b16636377f889f905c1e76cc095a61a41c4
Jonathan Cutrell <jonathan.cutrell@gmail.com>
2013-05-21 13:47:02 -0400

    progress

--- js/script.js
+++ js/script.js
@@ -4,4 +4,21 @@
 */
 (function($,w,undefined){
 
+$.getJSON("http://ourcog.com/api/core/get_category_posts/?slug=okcog&callback=?", function(data){
+	console.log(data);
+});
+
+$("textarea,input").not("[type=submit]").each(function(i,el){ $(el).data("val", $(this).val()); });
+$("body").on("focus", "textarea,input", function(){
+	if ($(this).val() == $(this).data("val") && $(this).not("[type=submit]")){
+		$(this).val("");
+	}
+}).on("blur", "textarea,input", function(e){
+	if ($.trim($(this).val()) == ""){
+		$(this).val($(this).data("val"));
+	}
+});
+
+
+
 }(jQuery, window));
